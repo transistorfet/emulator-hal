10000669524747dce101947e60f230551b26f6f8
transistor fet <trans@jabberwocky.ca>
2024-03-19 19:41:33 -0700

    Improved Instant and Inspect/Debug traits (#2)
    * Improved Instant and Inspect/Debug traits
    
    * Fixed fugit impl
    
    * Fixed pull request triggers

--- emulator-hal/src/time.rs
+++ emulator-hal/src/time.rs
@@ -1,11 +1,18 @@
 //! Traits and implementations for coordinating time between emulated components
 
-use core::ops::Add;
+use core::ops::{Add, Mul};
+use core::time::Duration;
 
 /// Represents a monotonic instant in time
-pub trait Instant: Add<Self::Duration> {
+pub trait Instant: Add<Self::Duration, Output = Self> + Copy {
+    /// The start of the epoch according to this time representation
+    const START: Self;
+
     /// Represents a duration that can be added to an instant of this type
-    type Duration;
+    type Duration: Mul<u32, Output = Self::Duration>;
+
+    /// Returns the duration of one period of the given frequency is hertz
+    fn hertz_to_duration(hertz: u64) -> Self::Duration;
 }
 
 /*
@@ -22,22 +29,53 @@ impl<T: Instant> InstantType for T {
 }
 */
 
-#[cfg(feature = "std")]
-impl Instant for std::time::Instant {
-    type Duration = std::time::Duration;
+impl Instant for Duration {
+    const START: Self = Duration::from_nanos(0);
+
+    type Duration = Duration;
+
+    fn hertz_to_duration(hertz: u64) -> Self::Duration {
+        Duration::from_nanos(1_000_000_000 / hertz)
+    }
 }
 
 #[cfg(feature = "fugit")]
-impl<T, const NOM: u32, const DENOM: u32> Instant for fugit::Instant<T, NOM, DENOM>
+impl<const NOM: u32, const DENOM: u32> Instant for fugit::Instant<u32, NOM, DENOM>
 where
-    Self: Add<fugit::Duration<T, NOM, DENOM>>,
+    Self: Add<fugit::Duration<u32, NOM, DENOM>, Output = Self>,
 {
-    type Duration = fugit::Duration<T, NOM, DENOM>;
+    const START: Self = fugit::Instant::<u32, NOM, DENOM>::from_ticks(0);
+
+    type Duration = fugit::Duration<u32, NOM, DENOM>;
+
+    fn hertz_to_duration(hertz: u64) -> Self::Duration {
+        fugit::Duration::<u32, NOM, DENOM>::from_ticks(DENOM / hertz as u32)
+    }
+}
+
+#[cfg(feature = "fugit")]
+impl<const NOM: u32, const DENOM: u32> Instant for fugit::Instant<u64, NOM, DENOM>
+where
+    Self: Add<fugit::Duration<u64, NOM, DENOM>, Output = Self>,
+{
+    const START: Self = fugit::Instant::<u64, NOM, DENOM>::from_ticks(0);
+
+    type Duration = fugit::Duration<u64, NOM, DENOM>;
+
+    fn hertz_to_duration(hertz: u64) -> Self::Duration {
+        fugit::Duration::<u64, NOM, DENOM>::from_ticks(DENOM as u64 / hertz)
+    }
 }
 
 #[cfg(feature = "femtos")]
 impl Instant for femtos::Instant {
+    const START: Self = femtos::Instant::START;
+
     type Duration = femtos::Duration;
+
+    fn hertz_to_duration(hertz: u64) -> Self::Duration {
+        femtos::Duration::from_femtos(1_000_000_000_000_000 / hertz as femtos::Femtos)
+    }
 }
 
 #[cfg(test)]
